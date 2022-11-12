2f3b0d86320045fd6cadb8bad8a9c4d1413ed11d
imamhossain94 <imamagun94@gmail.com>
2022-11-12 22:27:30 +0600

    feat: menu views widgets are seperated fron the main view

--- lib/app/view/menu_view.dart
+++ lib/app/view/menu_view.dart
@@ -10,6 +10,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../services/get_storage_service.dart';
 import '../utils/constants.dart';
 import '../utils/system_overlay.dart';
+import 'widgets/menu_item.dart';
 
 class MenuView extends StatelessWidget {
   const MenuView({Key? key}) : super(key: key);
@@ -196,53 +197,5 @@ class MenuView extends StatelessWidget {
     );
   }
 
-  Widget menuItem(
-      {required String title,
-      String? description,
-      Widget? icon,
-      VoidCallback? onTap, required Color bgColor,
-      required int duration}) {
-    return FadeInUp(
-      duration: Duration(milliseconds: duration),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.sp, vertical: 5.sp),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: onTap,
-            borderRadius: BorderRadius.circular(8.sp),
-            child: Container(
-              padding: EdgeInsets.all(15.sp),
-              decoration: BoxDecoration(
-                  color: bgColor,
-                  borderRadius: BorderRadius.circular(8.sp),
-                  border: Border.all(color: bgColor)),
-                  // border: Border.all(color: const Color(0xFFF1F3F2))),
-              child: Row(
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                        fontSize: 12.sp,
-                        color: Colors.black87,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  const Spacer(),
-                  if (description != null)
-                    Text(
-                      description,
-                      style: TextStyle(
-                          fontSize: 12.sp,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w200),
-                    ),
-                  if (icon != null) icon
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+
 }
---
+++ lib/app/view/widgets/menu_item.dart
@@ -0,0 +1,54 @@
+
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+Widget menuItem(
+    {required String title,
+      String? description,
+      Widget? icon,
+      VoidCallback? onTap, required Color bgColor,
+      required int duration}) {
+  return FadeInUp(
+    duration: Duration(milliseconds: duration),
+    child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.sp, vertical: 5.sp),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(8.sp),
+          child: Container(
+            padding: EdgeInsets.all(15.sp),
+            decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: BorderRadius.circular(8.sp),
+                border: Border.all(color: bgColor)),
+            // border: Border.all(color: const Color(0xFFF1F3F2))),
+            child: Row(
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                      fontSize: 12.sp,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.normal),
+                ),
+                const Spacer(),
+                if (description != null)
+                  Text(
+                    description,
+                    style: TextStyle(
+                        fontSize: 12.sp,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w200),
+                  ),
+                if (icon != null) icon
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
